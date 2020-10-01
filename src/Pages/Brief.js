7670d96f12c9ada1e082971e1183dfeba41897ec
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-10-01 12:02:40 -0500

    Brief Logs

--- src/Pages/Brief.js
+++ src/Pages/Brief.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
 import { MyContext } from "../context";
 import { Redirect } from "react-router-dom";
-import { Button, Form, Input, Card } from "antd";
+import { Button, Form, Input, Card, Tag, Typography } from "antd";
 import MY_SERVICE from "../services";
+
+const { Title } = Typography;
 
 const { getUserInfo, requestEmployer } = MY_SERVICE;
 
@@ -38,6 +40,30 @@ const Brief = () => {
     setmessage("Please wait to be validated");
   };
 
+  function getTime(timestamp) {
+    const date = new Date(timestamp);
+    const day =
+      date.getDay() === 0
+        ? "Sun"
+        : date.getDay() === 1
+        ? "Mon"
+        : date.getDay() === 2
+        ? "Tue"
+        : date.getDay() === 3
+        ? "Wed"
+        : date.getDay() === 4
+        ? "Thu"
+        : date.getDay() === 5
+        ? "Fri"
+        : date.getDay() === 6
+        ? "Sat"
+        : "";
+
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    return [day, hour, min];
+  }
+
   return user ? (
     user.role === "employer" ? (
       <div>
@@ -50,7 +76,7 @@ const Brief = () => {
         <br />
         <h2>{user.email}</h2>
         <br />
-        <h3>Share this code with collabs</h3>
+        <Title level={2}>Share this code with collabs</Title>
         <p>{user._id}</p>
         <br />
         <br />
@@ -90,14 +116,35 @@ const Brief = () => {
           <></>
         )}
         <p>{message}</p>
-        <h3>Recent Logs</h3>
+        <Title level={2}>Recent Logs</Title>
         {logs ? (
           logs.map((log, ind) => (
-            <Card title={log.poi.name} style={{ marginTop: "5" }}>
-              <p>{log.createdAt}</p>
+            <Card
+              title={log.poi ? log.poi.name : "Dropped PoI"}
+              style={{ marginTop: 16 }}
+            >
+              <div style={{ marginBottom: 10 }}>
+                <Title level={5}>Check In by: {log.poi.checkinTime}</Title>
+                {log.poi.weekdays.map((tag, ind) => (
+                  <span>
+                    <Tag key={ind}>{tag}</Tag>
+                  </span>
+                ))}
+              </div>
+              <div>
+                <Title level={5}>Log Info</Title>
+                <span>
+                  Day: {getTime(log.createdAt)[0]}
+                  {"  "}
+                </span>
+                <span>|</span>
+                <span>
+                  Time: {getTime(log.createdAt)[1]}:{getTime(log.createdAt)[2]}
+                </span>
+              </div>
               <p>
                 Valid:{" "}
-                {log.valid ? (
+                {log.valid === true ? (
                   <strong style={validStyles}>Validated</strong>
                 ) : (
                   <strong style={unvalidStyles}>Not Validated</strong>
@@ -106,7 +153,10 @@ const Brief = () => {
             </Card>
           ))
         ) : (
-          <h2>Loading</h2>
+          <div>
+            <h2>No logs to show</h2>
+            <p>Make sure the scanned QRCodes are still valid</p>
+          </div>
         )}
       </div>
     )
